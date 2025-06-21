6d40c7f0a5a10b68b8699d0bc1d39d3dd1fc46d1
John <tukhtamirzaevnazirjon96@gmail.com>
2025-06-21 15:31:43 +0900

    feat: connect database

---
+++ notes.ts
@@ -0,0 +1,5 @@
+/*
+1. CLUSTER(Mongo_Atlas_John) => DATABASE(Seller) => COLLECTION(members vh) => DOCUMENT(birdona data)
+2. git log --oneline => oxirgi commitni ko'rish
+3. git reset --hard => oxirgi commitga qaytish
+*/
