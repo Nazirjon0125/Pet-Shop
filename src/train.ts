5c029c31734571855a4cf9c8856c6ed5a273f713
John <tukhtamirzaevnazirjon96@gmail.com>
2025-11-09 01:54:14 +0900

    feat: deploy project via shell & pm2

---
+++ src/train.ts
@@ -0,0 +1,12 @@
+/*
+PM2 COMMANDS
+
+pm2 ls
+pm2 start dist/server.js --name=PETSHOP
+pm2 start "pnm run start:prod" --name=PETSHOP
+pm2 stop id
+pm2 delete id
+pm2 restart id
+pm2 monit
+pm2 kill
+*/
